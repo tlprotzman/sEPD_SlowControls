ca27354ea633a15c7d81045f0e8bd71fa9d98248
Change Me <changeme@bnl.gov>
2023-08-06 14:34:10 -0400

    remove unused files

--- install_daq02_root.sh
+++ install_daq02_root.sh
@@ -2,13 +2,9 @@
 
 echo "Installation/update starts .... (requiring root account)"
 
-cp -fv sepd_server.service /usr/lib/systemd/system/sepd_server.service
 cp -fv sepd_exporter.service /usr/lib/systemd/system/sepd_exporter.service
 
 systemctl daemon-reload
-
-systemctl restart sepd_server.service
-systemctl enable sepd_server.service
 
 systemctl restart sepd_exporter.service
 systemctl enable sepd_exporter.service
